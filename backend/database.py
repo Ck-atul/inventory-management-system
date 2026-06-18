c553b7c492a3a1a89354b4ef0d492215d6ef117b
Ck-atul <atul@connecwrk.com>
2026-06-18 20:21:48 +0530

    Add fallback Database URL and EXPOSE 8000 for Docker Desktop testing

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/inventory_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:YDqQgXWObhVUheYgZSPkSOtAQMupmvHg@thomas.proxy.rlwy.net:34213/railway")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
